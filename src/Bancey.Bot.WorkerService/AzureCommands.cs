26f4f0123bb5677e966fd8af364c4054df9ae312
Alex Bance <abance@bancey.xyz>
2024-05-10 20:11:00 +0100

    fix: validation exceptions

--- src/Bancey.Bot.WorkerService/AzureCommands.cs
+++ src/Bancey.Bot.WorkerService/AzureCommands.cs
@@ -10,129 +10,145 @@ namespace Bancey.Bot.WorkerService;
 [Group("azure", "Azure Commands")]
 public class AzureCommands(ILogger<BanceyBotInteractionModuleBase> logger, TelemetryClient telemetryClient, IConfiguration configuration, AzureClient azureClient) : BanceyBotInteractionModuleBase(logger, telemetryClient, configuration)
 {
-  private readonly AzureClient _azureClient = azureClient;
+    private readonly AzureClient _azureClient = azureClient;
 
-  [SlashCommand("list-servers", "Lists servers matching configured tags.")]
-  public async Task GetServers()
-  {
-    if (_settings == null || _settings.Azure == null || _settings.Azure.Tags == null || _settings.Azure.Tags.Count == 0)
+    [SlashCommand("list-servers", "Lists servers matching configured tags.")]
+    public async Task GetServers()
     {
-      _logger.LogError("Required Azure settings not found.");
-      await RespondAsync("Please contact the bot owner, required settings are missing.");
-      return;
+        if (_settings == null || _settings.Azure == null || _settings.Azure.Tags == null || _settings.Azure.Tags.Count == 0)
+        {
+            _logger.LogError("Required Azure settings not found.");
+            await RespondAsync("Please contact the bot owner, required settings are missing.");
+            return;
+        }
+
+        var valid = await ValidateUser();
+        if (!valid)
+        {
+            _logger.LogError("User {user} in Channel {channel} is not authorized to run this command.", Context.User.GlobalName, Context.Interaction.ChannelId);
+            await RespondAsync("You or this channel are not authorised to run this command. Please contact the bot owner.");
+            return;
+        }
+
+        var formattedTags = _settings.Azure.Tags.Select(tag => $"{tag.Key}={tag.Value}");
+
+        var getServersOperation = _telemetryClient.StartOperation<RequestTelemetry>("GetServers");
+        _logger.LogInformation("Getting servers matching configured tags. {tags}", formattedTags);
+        await DeferAsync();
+        var subscription = await _azureClient.GetSubscription();
+        var virtualMachines = await _azureClient.GetVirtualMachines(subscription, _settings.Azure.Tags);
+
+        var embed = new EmbedBuilder()
+          .WithTitle("Azure Servers")
+          .WithColor(Color.DarkTeal);
+
+        var stringBuilder = new StringBuilder();
+        foreach (var virtualMachine in virtualMachines)
+        {
+            stringBuilder
+              .AppendLine($"### {virtualMachine.Data.Name}")
+              .AppendLine($"**Status**: `{virtualMachine.Get().Value.InstanceView().Value.Statuses[1].DisplayStatus}`");
+        }
+        stringBuilder.AppendLine();
+        var commandId = (await Context.Guild.GetApplicationCommandsAsync()).FirstOrDefault(cmd => cmd.Name == "azure")?.Id;
+        stringBuilder.AppendLine($"Start a server: </azure start-server:{commandId}>");
+
+        embed.WithDescription(stringBuilder.ToString()).WithFooter($"Total: {virtualMachines.Count}, Tags: {string.Join(", ", formattedTags)}").WithCurrentTimestamp();
+        await FollowupAsync(embeds: [embed.Build()]);
+        _telemetryClient.StopOperation(getServersOperation);
     }
 
-    var valid = await ValidateUser();
-    if (!valid) {
-      _logger.LogError("User {user} in Channel {channel} is not authorized to run this command.", Context.User.GlobalName, Context.Channel.Name);
-      await RespondAsync("You or this channel are not authorised to run this command. Please contact the bot owner.");
-      return;
-    }
-
-    var formattedTags = _settings.Azure.Tags.Select(tag => $"{tag.Key}={tag.Value}");
-
-    var getServersOperation = _telemetryClient.StartOperation<RequestTelemetry>("GetServers");
-    _logger.LogInformation("Getting servers matching configured tags. {tags}", formattedTags);
-    await DeferAsync();
-    var subscription = await _azureClient.GetSubscription();
-    var virtualMachines = await _azureClient.GetVirtualMachines(subscription, _settings.Azure.Tags);
-
-    var embed = new EmbedBuilder()
-      .WithTitle("Azure Servers")
-      .WithColor(Color.DarkTeal);
-
-    var stringBuilder = new StringBuilder();
-    foreach (var virtualMachine in virtualMachines)
+    [SlashCommand("start-server", "Starts a specific Azure VM.")]
+    public async Task StartServer([Summary("serverName", "Attempts to start a server in Azure."), Autocomplete(typeof(VirtualMachineAutoCompleteHandler))] string serverName)
     {
-      stringBuilder
-        .AppendLine($"### {virtualMachine.Data.Name}")
-        .AppendLine($"**Status**: `{virtualMachine.Get().Value.InstanceView().Value.Statuses[1].DisplayStatus}`");
+        var valid = await ValidateUser();
+        if (!valid)
+        {
+            _logger.LogError("User {user} in Channel {channel} is not authorized to run this command.", Context.User.GlobalName, Context.Interaction.ChannelId);
+            await RespondAsync("You or this channel are not authorised to run this command. Please contact the bot owner.");
+            return;
+        }
+        var startServerOperation = _telemetryClient.StartOperation<RequestTelemetry>("StartServer");
+        _logger.LogInformation("Starting server {serverName}", serverName);
+        await DeferAsync();
+        var subscription = await _azureClient.GetSubscription();
+        var virtualMachine = await _azureClient.GetVirtualMachineAsync(subscription, serverName);
+
+        if (virtualMachine == null)
+        {
+            await FollowupAsync($"No server found with the name {serverName}.");
+            _telemetryClient.StopOperation(startServerOperation);
+            return;
+        }
+
+        var result = await virtualMachine.PowerOnAsync(WaitUntil.Started);
+        _logger.LogInformation("Server {serverName} starting...", serverName);
+        await FollowupAsync($"Server {serverName} starting up...");
+
+        while (result.HasCompleted == false)
+        {
+            _logger.LogInformation("Waiting for server {serverName} to start...", serverName);
+            await Task.Delay(5000);
+            await result.UpdateStatusAsync();
+        }
+
+        _logger.LogInformation("Server {serverName} started.", serverName);
+        await FollowupAsync($"Server {serverName} started.");
+        _telemetryClient.StopOperation(startServerOperation);
     }
-    stringBuilder.AppendLine();
-    var commandId = (await Context.Guild.GetApplicationCommandsAsync()).FirstOrDefault(cmd => cmd.Name == "azure")?.Id;
-    stringBuilder.AppendLine($"Start a server: </azure start-server:{commandId}>");
 
-    embed.WithDescription(stringBuilder.ToString()).WithFooter($"Total: {virtualMachines.Count}, Tags: {string.Join(", ", formattedTags)}").WithCurrentTimestamp();
-    await FollowupAsync(embeds: [embed.Build()]);
-    _telemetryClient.StopOperation(getServersOperation);
-  }
-
-  [SlashCommand("start-server", "Starts a specific Azure VM.")]
-  public async Task StartServer([Summary("serverName", "Attempts to start a server in Azure."), Autocomplete(typeof(VirtualMachineAutoCompleteHandler))] string serverName)
-  {
-    var startServerOperation = _telemetryClient.StartOperation<RequestTelemetry>("StartServer");
-    _logger.LogInformation("Starting server {serverName}", serverName);
-    await DeferAsync();
-    var subscription = await _azureClient.GetSubscription();
-    var virtualMachine = await _azureClient.GetVirtualMachineAsync(subscription, serverName);
-
-    if (virtualMachine == null)
+    [SlashCommand("stop-server", "Stops a specific Azure VM.")]
+    public async Task StopServer([Summary("serverName", "Attempts to stop a server in Azure."), Autocomplete(typeof(VirtualMachineAutoCompleteHandler))] string serverName)
     {
-      await FollowupAsync($"No server found with the name {serverName}.");
-      _telemetryClient.StopOperation(startServerOperation);
-      return;
+        var valid = await ValidateUser();
+        if (!valid)
+        {
+            _logger.LogError("User {user} in Channel {channel} is not authorized to run this command.", Context.User.GlobalName, Context.Interaction.ChannelId);
+            await RespondAsync("You or this channel are not authorised to run this command. Please contact the bot owner.");
+            return;
+        }
+
+        var stopServerOperation = _telemetryClient.StartOperation<RequestTelemetry>("StopServer");
+        _logger.LogInformation("Shutting down server {serverName} gracefully.", serverName);
+        await DeferAsync();
+        var subscription = await _azureClient.GetSubscription();
+        var virtualMachine = await _azureClient.GetVirtualMachineAsync(subscription, serverName);
+
+        if (virtualMachine == null)
+        {
+            await FollowupAsync($"No server found with the name {serverName}.");
+            _telemetryClient.StopOperation(stopServerOperation);
+            return;
+        }
+
+        var result = await virtualMachine.PowerOffAsync(WaitUntil.Started, false);
+        _logger.LogInformation("Server {serverName} shutting down...", serverName);
+        await FollowupAsync($"Server {serverName} shutting down...");
+
+        await result.UpdateStatusAsync();
+
+        while (result.HasCompleted == false)
+        {
+            _logger.LogInformation("Waiting for server {serverName} to shutdown...", serverName);
+            await Task.Delay(5000);
+            await result.UpdateStatusAsync();
+        }
+
+        _logger.LogInformation("Server {serverName} shutdown.", serverName);
+        await FollowupAsync($"Server {serverName} shutdown... Now deallocating resources.");
+
+        var deallocateResult = await virtualMachine.DeallocateAsync(WaitUntil.Started);
+        _logger.LogInformation("Server {serverName} deallocating.", serverName);
+
+        while (deallocateResult.HasCompleted == false)
+        {
+            _logger.LogInformation("Waiting for server {serverName} to deallocate...", serverName);
+            await Task.Delay(5000);
+            await deallocateResult.UpdateStatusAsync();
+        }
+
+        _logger.LogInformation("Server {serverName} deallocated.", serverName);
+        await FollowupAsync($"Server {serverName} deallocated.");
+        _telemetryClient.StopOperation(stopServerOperation);
     }
-
-    var result = await virtualMachine.PowerOnAsync(WaitUntil.Started);
-    _logger.LogInformation("Server {serverName} starting...", serverName);
-    await FollowupAsync($"Server {serverName} starting up...");
-
-    while (result.HasCompleted == false)
-    {
-      _logger.LogInformation("Waiting for server {serverName} to start...", serverName);
-      await Task.Delay(5000);
-      await result.UpdateStatusAsync();
-    }
-
-    _logger.LogInformation("Server {serverName} started.", serverName);
-    await FollowupAsync($"Server {serverName} started.");
-    _telemetryClient.StopOperation(startServerOperation);
-  }
-
-  [SlashCommand("stop-server", "Stops a specific Azure VM.")]
-  public async Task StopServer([Summary("serverName", "Attempts to stop a server in Azure."), Autocomplete(typeof(VirtualMachineAutoCompleteHandler))] string serverName)
-  {
-    var stopServerOperation = _telemetryClient.StartOperation<RequestTelemetry>("StopServer");
-    _logger.LogInformation("Shutting down server {serverName} gracefully.", serverName);
-    await DeferAsync();
-    var subscription = await _azureClient.GetSubscription();
-    var virtualMachine = await _azureClient.GetVirtualMachineAsync(subscription, serverName);
-
-    if (virtualMachine == null)
-    {
-      await FollowupAsync($"No server found with the name {serverName}.");
-      _telemetryClient.StopOperation(stopServerOperation);
-      return;
-    }
-
-    var result = await virtualMachine.PowerOffAsync(WaitUntil.Started, false);
-    _logger.LogInformation("Server {serverName} shutting down...", serverName);
-    await FollowupAsync($"Server {serverName} shutting down...");
-
-    await result.UpdateStatusAsync();
-
-    while (result.HasCompleted == false)
-    {
-      _logger.LogInformation("Waiting for server {serverName} to shutdown...", serverName);
-      await Task.Delay(5000);
-      await result.UpdateStatusAsync();
-    }
-
-    _logger.LogInformation("Server {serverName} shutdown.", serverName);
-    await FollowupAsync($"Server {serverName} shutdown... Now deallocating resources.");
-
-    var deallocateResult = await virtualMachine.DeallocateAsync(WaitUntil.Started);
-    _logger.LogInformation("Server {serverName} deallocating.", serverName);
-
-    while (deallocateResult.HasCompleted == false)
-    {
-      _logger.LogInformation("Waiting for server {serverName} to deallocate...", serverName);
-      await Task.Delay(5000);
-      await deallocateResult.UpdateStatusAsync();
-    }
-
-    _logger.LogInformation("Server {serverName} deallocated.", serverName);
-    await FollowupAsync($"Server {serverName} deallocated.");
-    _telemetryClient.StopOperation(stopServerOperation);
-  }
 }
--- src/Bancey.Bot.WorkerService/BanceyBotInteractionModuleBase.cs
+++ src/Bancey.Bot.WorkerService/BanceyBotInteractionModuleBase.cs
@@ -5,65 +5,56 @@ namespace Bancey.Bot.WorkerService;
 
 public abstract class BanceyBotInteractionModuleBase : InteractionModuleBase
 {
-  internal readonly ILogger<BanceyBotInteractionModuleBase> _logger;
-  internal readonly TelemetryClient _telemetryClient;
-  internal readonly BanceyBotSettings? _settings;
+    internal readonly ILogger<BanceyBotInteractionModuleBase> _logger;
+    internal readonly TelemetryClient _telemetryClient;
+    internal readonly BanceyBotSettings? _settings;
 
-  public BanceyBotInteractionModuleBase(ILogger<BanceyBotInteractionModuleBase> logger, TelemetryClient telemetryClient, IConfiguration configuration)
-  {
-    _logger = logger;
-    _telemetryClient = telemetryClient;
-    _settings = configuration.GetRequiredSection("BanceyBot").Get<BanceyBotSettings>();
-  }
-
-  public override void BeforeExecute(ICommandInfo command)
-  {
-    _logger.LogInformation("Executing command: {command}", command.Name);
-    base.BeforeExecute(command);
-  }
-
-  public override void AfterExecute(ICommandInfo command)
-  {
-    _logger.LogInformation("Finished executing command: {command}", command.Name);
-    base.AfterExecute(command);
-  }
-
-  internal async Task<bool> ValidateUser()
-  {
-    if (_settings == null)
+    public BanceyBotInteractionModuleBase(ILogger<BanceyBotInteractionModuleBase> logger, TelemetryClient telemetryClient, IConfiguration configuration)
     {
-      _logger.LogInformation("Unable to validate user, settings are null.");
-      return true;
+        _logger = logger;
+        _telemetryClient = telemetryClient;
+        _settings = configuration.GetRequiredSection("BanceyBot").Get<BanceyBotSettings>();
     }
 
-    bool validChannel = true;
-    bool validRoles = true;
-
-    if (_settings.AllowedChannelIds != null && _settings.AllowedChannelIds.Count > 0)
+    public override void BeforeExecute(ICommandInfo command)
     {
-      string channelId = Context.Channel.Id.ToString();
-      _logger.LogInformation("Checking if channel {channel} is valid...", channelId);
-      try
-      {
-        validChannel = _settings.AllowedChannelIds.Contains(channelId);
-      }
-      catch (Exception ex)
-      {
-        _logger.LogError("Error checking channel {channel}: {error}", channelId, ex.Message);
-        _logger.LogInformation("Type {}", ex.GetType().FullName);
-      }
-
+        _logger.LogInformation("Executing command: {command}", command.Name);
+        base.BeforeExecute(command);
     }
-    _logger.LogInformation("Channel {channel} is {valid}", $"{Context.Channel.Name}-{Context.Channel.Id}", validChannel);
 
-    if (_settings.AllowedRoleIds != null && _settings.AllowedRoleIds.Count > 0)
+    public override void AfterExecute(ICommandInfo command)
     {
-      _logger.LogInformation("Checking if user has the correct roles...");
-      var guildUser = await Context.Guild.GetUserAsync(Context.User.Id);
-      validRoles = _settings.AllowedRoleIds.All(allowedRole => guildUser.RoleIds.Contains(ulong.Parse(allowedRole)));
+        _logger.LogInformation("Finished executing command: {command}", command.Name);
+        base.AfterExecute(command);
     }
-    _logger.LogInformation("User {user} is {valid}", $"{Context.User.GlobalName}-{Context.User.Id}", validRoles);
 
-    return validChannel == true && validRoles == true;
-  }
+    internal async Task<bool> ValidateUser()
+    {
+        if (_settings == null)
+        {
+            _logger.LogInformation("Unable to validate user, settings are null.");
+            return true;
+        }
+
+        bool validChannel = true;
+        bool validRoles = true;
+
+        if (_settings.AllowedChannelIds != null && _settings.AllowedChannelIds.Count > 0 && Context.Interaction.ChannelId != null)
+        {
+            string channelId = Context.Interaction.ChannelId.ToString()!;
+            _logger.LogInformation("Checking if channel {channel} is valid...", channelId);
+            validChannel = _settings.AllowedChannelIds.Contains(channelId);
+        }
+        _logger.LogInformation("Channel {channel} is {valid}", Context.Interaction.ChannelId, validChannel ? "valid" : "not valid");
+
+        if (_settings.AllowedRoleIds != null && _settings.AllowedRoleIds.Count > 0)
+        {
+            _logger.LogInformation("Checking if user has the correct roles...");
+            var guildUser = await Context.Guild.GetUserAsync(Context.User.Id);
+            validRoles = _settings.AllowedRoleIds.All(allowedRole => guildUser.RoleIds.Contains(ulong.Parse(allowedRole)));
+        }
+        _logger.LogInformation("User {user} is {valid}", $"{Context.User.GlobalName}-{Context.User.Id}", validRoles);
+
+        return validChannel == true && validRoles == true;
+    }
 }
